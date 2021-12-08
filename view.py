cb411e95e456aca0d9cb8d712a0032461dce9ca2
Evan <dunneev@gmail.com>
2021-12-08 01:45:52 -0500

    GUI update

--- view.py
+++ view.py
@@ -1,20 +1,46 @@
-import tkinter as tk    # Standard binding to tk
+import tkinter as tk
+from tkinter.constants import ANCHOR    # Standard binding to tk
 import tkinter.ttk as ttk   # Binding to ttk submodule for new/prettier themed widgets
 
 # Create window 
 window = tk.Tk()
 window.title("ArticArtFetcher")
 
+# Create Main Content Frame
 mainframe = ttk.Frame(master=window)
-mainframe.grid(column=0, row=0, ipadx=200, ipady=200, sticky=(tk.N, tk.E, tk.S, tk.W))
+mainframe.grid(column=0, row=0, sticky=(tk.NSEW))
+
+# Handle resize proportions
+mainframe.columnconfigure(index=0, weight=1)
+mainframe.columnconfigure(index=1, weight=1)
+mainframe.rowconfigure(index=0, weight=3)
+mainframe.rowconfigure(index=1, weight=1)
+
 window.columnconfigure(index=0, weight=1)
 window.rowconfigure(index=0, weight=1)
 
-file_management_frame = ttk.Frame(mainframe, borderwidth=5, relief="ridge", width=200, height=100)
-file_management_frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(tk.N,tk.W))
 
-artwork_criteria_frame = ttk.Frame(mainframe, borderwidth=5, relief="ridge", width=200, height=100)
-artwork_criteria_frame.grid(column=1, row=0, columnspan=3, rowspan=2, sticky=(tk.N,tk.E))
+# Set up Frames
+file_management_frame = ttk.Frame(master=mainframe, borderwidth=5, relief=tk.RIDGE)
+file_management_frame.grid(column=0, row=0, sticky=(tk.NSEW))
+
+artwork_criteria_frame = ttk.Frame(master=mainframe, borderwidth=5, relief=tk.RIDGE)
+artwork_criteria_frame.grid(column=1, row=0, sticky=(tk.NSEW))
+
+log_frame = ttk.Frame(master=mainframe, borderwidth=5, relief=tk.RIDGE)
+log_frame.grid(column=0, row=1, sticky=(tk.NSEW))
+
+button_frame = ttk.Frame(master=mainframe)
+button_frame.grid(column=1, row=1)
+
+
+
+# Populate frames with labels
+ttk.Label(master=file_management_frame, text="File Management").grid()
+ttk.Label(master=artwork_criteria_frame, text="Artwork Criteria").grid()
+ttk.Label(master=log_frame, text="Log").grid()
+ttk.Button(master=button_frame, text="Fetch Art").grid()
+
 
 
 window.mainloop()
